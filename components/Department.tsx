4b47bc5c572807ccd5c47d5e7331a3054da3d243
Rayen-Belkhouja <rayen.belkhoujaa@gmail.com>
2024-10-03 13:07:26 +0100

    more fixes

--- components/Department.tsx
+++ components/Department.tsx
@@ -9,17 +9,17 @@ import { RiInstagramLine } from "react-icons/ri";
 export function ThreeDCardDemo({title, desc, image, instalink} : {title?:string; desc?:string; image?:string; instalink?:string}) {
   return (
     <CardContainer className="inter-var">
-      <CardBody className="bg-gray-50 mb-5 flex flex-col justify-between h-[550px] relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto  2xl:w-[40rem] lg:w-[32rem] rounded-xl p-5 border  ">
+      <CardBody className="bg-gray-50 mb-5 flex flex-col justify-between h-[550px] relative group/card   border-black/[0.1] w-auto  2xl:w-[40rem] lg:w-[32rem] rounded-xl p-5 border  ">
         <CardItem
           translateZ="50"
-          className="text-4xl text-university-yellow font-bold dark:text-white"
+          className="text-4xl text-university-yellow font-bold "
         >
           {title}
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
-          className="parag mt-2 dark:text-neutral-300"
+          className="parag mt-2 "
         >
           {desc}
         </CardItem>
@@ -35,7 +35,7 @@ export function ThreeDCardDemo({title, desc, image, instalink} : {title?:string;
         <CardItem
             translateZ={20}
             as="button"
-            className="p-2 rounded-full bg-university-yellow dark:bg-white dark:text-black text-white text-xs font-bold"
+            className="p-2 rounded-full bg-university-yellow  text-white text-xs font-bold"
           >
             <a href={instalink} target="_blank">
               <RiInstagramLine color='white' size={25} />
